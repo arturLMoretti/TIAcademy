8d9e9988794d615fb335c48032d2a2627b20e394
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-10-23 19:49:06 -0300

    Cria rotas

--- Ciclo 3/Aulas 1 a 4/Controller.js
+++ Ciclo 3/Aulas 1 a 4/Controller.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 
-const app = express;
-app.request(cors());
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Olá mundo')
+});
+
+app.get('/clientes', (req, res) => {
+    res.send('Seja bem vindo(a) ao ServiceTI!')
+});
+
+app.get('/servicos', (req, res) => {
+    res.send('Esta é a área de Serviços! Sinta-se a vontade!')
+});
+
+app.get('/pedidos', (req, res) => {
+    res.send('Esta é a área de pedidos! Vamos encher o carrinho?')
+});
 
 let port = process.env.PORT || 3001;
 
